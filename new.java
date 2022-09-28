6caa2916165a8ceb6a374995d46a7272094729c2
abdulrahim13 <56230780+abdulrahim13@users.noreply.github.com>
2022-09-28 05:54:35 -0700

    Update new.java

--- new.java
+++ new.java
@@ -1,17 +1,14 @@
- webView.setWebViewClient(new WebViewClient(){
-            @Override
-            public boolean shouldOverrideUrlLoading(WebView view, String url) {
-                view.loadUrl(url);
+
+  webView.setWebViewClient(new MyWebViewClient());
+
+private class MyWebViewClient extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            if (url.equals("www.example.com")) {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+                startActivity(intent);
                 return true;
             }
-            @Override
-            public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
-                super.onReceivedError(view, errorCode, description, failingUrl);
-            }
-
-            @SuppressLint("WebViewClientOnReceivedSslError")
-            @Override
-            public void onReceivedSslError(WebView view, SslErrorHandler handler, SslError error) {
-                handler.proceed();
-            }
-        });
+            return false;
+        }
+    }
